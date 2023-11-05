a433b3ebb41795a356ddb676ed92c0f4c915aaa8
Dan-Mihai-Banea <danbanea1996@gmail.com>
2023-11-05 17:44:25 +0200

    Commit[002]:Fixed the Logo component

--- src/components/homepage/Logo.jsx
+++ src/components/homepage/Logo.jsx
@@ -1,6 +1,6 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import styled from 'styled-components';
-import image1 from '../../media/logo/logo-no-background.png'; // Import your PNG images
+import image1 from '../../media/logo/logo-no-background.png';
 import image2 from '../../media/logo/logo-leafs.png';
 
 const LogoContainer = styled.div`
@@ -20,8 +20,8 @@ const Image1 = styled.img`
   position: absolute;
   // top: 50%;
   // left: 50%;
-  // transform: translate(-50%, -50%);
-  transition: transform 0.2s ease-in-out;
+  transform: translate(-50%, -50%);
+  transition: transform 0.05s ease-in-out;
   height: 125px;
   width: 125px;
   // z-index: 9999999999999;
@@ -31,12 +31,14 @@ const Image2 = styled.img`
   position: absolute;
   // top: 50%;
   // left: 50%;
-  // transform: translate(-50%, -50%);
-  transition: transform 0.2s ease-in-out;
+  transform: translate(-50%, -50%);
+  transition: transform 0.05s ease-in-out;
   height: 200px;
   width: 200px;
   // z-index: 9999999999999;
 `;
+
+// let resetTimeout;
 
 const Logo = () => {
   const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
@@ -63,7 +65,28 @@ const Logo = () => {
 
     setMousePosition({ x: mouseX, y: mouseY });
     setImagePosition({ x: newX, y: newY });
+
+    // Clear any previous resetTimeout
+    // if (resetTimeout) {
+    //   clearTimeout(resetTimeout);
+    // }
   };
+
+  useEffect(() => {
+    const container = containerRef.current;
+
+    const handleMouseLeave = () => {
+      // When the cursor leaves the container, reset the images
+      setImagePosition({ x: 0, y: 0 });
+    };
+
+    container.addEventListener('mouseleave', handleMouseLeave);
+
+    return () => {
+      // Remove the event listener when the component unmounts
+      container.removeEventListener('mouseleave', handleMouseLeave);
+    };
+  }, []);
 
   return (
     <LogoContainer ref={containerRef} onMouseMove={handleMouseMove}>
